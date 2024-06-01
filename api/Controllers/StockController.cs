dfe01db3ebfedad09f30b366cff4df3af0753246
Ramil Valiyev <ramil4360616@gmail.com>
2024-06-01 16:59:07 +0400

    Post request has been created for Stock

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Dtos.Stock;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
@@ -35,6 +36,15 @@ namespace api.Controllers
             }
             
             return Ok(stocks.ToStockDto());
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateStockRequestDto stockDto)
+        {
+            var stockModel = stockDto.ToStockFromCreateDTO();
+            _context.Stock.Add(stockModel);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new {id=stockModel.Id}, stockModel.ToStockDto());
         }
 
     }
