f098fab4634254fd64372047460f94b4b32d6c0c
Ramil Valiyev <ramil4360616@gmail.com>
2024-06-01 18:20:00 +0400

    Delete method has been created from Stock

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -72,6 +72,23 @@ namespace api.Controllers
 
         }
 
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var stockModel = _context.Stock.FirstOrDefault(x => x.Id == id);
+
+            if(stockModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Stock.Remove(stockModel);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
 
     }
 }
